b50419a8d88e1ea8d6c5164c9383d455106c3e94
kamyar kamali kamazani <kamyarkamali021@gmail.com>
2023-06-25 03:32:59 +0330

    started project resturant

---
+++ Components/layout/Layout.js
@@ -0,0 +1,100 @@
+import React, { useEffect, useState } from 'react'
+import Link from "next/Link"
+import Image from 'next/image';
+
+//Labale
+import resturan from "../../public/assets/restoran.jpg"
+
+///Icons
+import {BsBag} from "react-icons/bs"
+import { BsList } from 'react-icons/bs';
+import {FiInstagram} from "react-icons/fi";
+import {IoLogoLinkedin} from "react-icons/io"
+import { FiMapPin } from 'react-icons/fi';
+
+function Layout({children}) {
+  const [isMobile,setIsMobile]=useState(false);
+  const [open,setIsOpen]=useState(false);
+
+  useEffect(()=>{
+    const handelres=()=>{
+      if(window.innerWidth<768){
+        setIsMobile(true)
+      }else{
+        setIsMobile(false)
+      }
+    }
+
+    window.addEventListener("resize",handelres)
+    return()=>{
+      window.removeEventListener("resize",handelres)
+    }
+
+    
+  },[])
+  
+  const openHamdeler=()=>{
+    setIsOpen(!open)
+  }
+  return (
+    <>
+    <header>
+      <div className='flex justify-between p-[12px] items-center bg-orange-500 max-w-[1419px] mx-auto'>
+        <div className='flex flex-col items-center'>
+          <p className='text-2xl text-white'>با ما در تماس باشید</p>
+          <p className='font-bold py-3 text-yellow-300 text-xl'>021-44444444</p>
+        </div>
+        <div>
+          {!isMobile&& (
+            <div className='flex gap-4 text-white font-bold'> 
+              <button className='bg-green-600 p-1 rounded-lg'>ورود</button>
+              <button className='bg-blue-600 p-1 rounded-lg'>ثبت نام</button>
+            </div>
+          )
+          
+          }
+        </div>
+        {!isMobile&&<div className='relative'>
+          <BsBag size={20} color='blue'/>
+          <span className='absolute top-[-10px] bg-red-500 p-[2px] h-[27px] w-[15px] text-center rounded-[50%] right-[-11px] text-white'>0</span>
+        </div>}
+        <div className='relative'>
+          {!isMobile ?<Link href={"/"}>
+          <Image src={resturan} width={60} height={60} className='rounded-[50%] object-cover'/>
+          </Link> : <BsList onClick={openHamdeler} size={30} className='cursor-pointer'/>}
+          {isMobile&&<div className={open ? 'absolute bg-orange-600 p-[4px] text-white font-bold right-[-14px] rounded-xl w-[200px] h-screen duration-300' : "absolute bg-orange-600 p-[4px] text-white font-bold right-[700px] rounded-xl w-[200px] h-screen duration-300"}>
+            <ul className='flex flex-col items-center p-4'>
+              <li className='py-6 hover:scale-105 duration-300 cursor-pointer'>صفحه اصلی</li>
+              <li className='py-6 hover:scale-105 duration-300 cursor-pointer'>سبد خرید</li>
+              <li className='py-6 hover:scale-105 duration-300 cursor-pointer'>تمام غذاها</li>
+              <li className='py-6 hover:scale-105 duration-300 cursor-pointer'>درباره ما</li>
+              <li className='py-6 hover:scale-105 duration-300 cursor-pointer'> تماس با ما</li>
+            </ul>
+            <div className='flex justify-center gap-6'>
+              <button className='bg-green-600 p-1 rounded-xl'>ورود</button>
+              <button className='bg-blue-600 p-1 rounded-xl'>ثبت نام</button>
+            </div>
+            <div className='flex items-center py-4 flex-col'>
+              <FiMapPin className='ml-[8rem]'/>
+              <p className='text-sm py-2 font-bold text-blue-300'>تهران,شهرک غرب,ایران زمین</p>
+            </div>
+            <div className='flex flex-col items-center'>
+              <p>:با ما همراه باشید</p>
+              <div className='flex py-4 gap-3'>
+                <FiInstagram size={24} className='cursor-pointer'/>
+                <IoLogoLinkedin size={24} color='blue' className='cursor-pointer'/>
+              </div>
+            </div>
+          </div>}
+        </div>
+      </div>
+    </header>
+
+      <div className='min-h-[900px]'>{children}</div>
+
+    <div>footer</div>
+    </>
+  )
+}
+
+export default Layout
